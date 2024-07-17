78f55fc30f0ee2a10e538d40d215aefe320a3942
Deep-Dey1 <deepdey715@gmail.com>
2024-07-17 22:17:46 +0530

    main.py updated dataset information

--- Project1_MovieGenreClassification/main.py
+++ Project1_MovieGenreClassification/main.py
@@ -1,6 +1,6 @@
 # LinkedIn Profile: www.linkedin.com/in/deep-dey-959590270/
 # refrnce GitHub Repository: https://github.com/Deep-Dey1/CODSOFT/tree/main/Project1_MovieGenreClassification
-
+# The data sets are taken from kaggle: https://www.kaggle.com/datasets/hijest/genre-classification-dataset-imdb
 
 # Lets start the code by importing necessary libraries
 import pandas as pd
